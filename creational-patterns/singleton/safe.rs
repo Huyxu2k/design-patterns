cc54a7704204b657258753f01e2d8d9367a8bd51
huyxu2k <nguyenhuy2712000@gmail.com>
2024-02-12 21:05:32 +0700

    singleton

---
+++ creational-patterns/singleton/safe.rs
@@ -0,0 +1,14 @@
+use std::arch::global_asm;
+
+
+fn change(global_state: &mut u32){
+    *global_state+=1;
+}
+
+fn main(){
+    let mut global_state=0u32;
+
+    change(&mut global_state);
+
+    println!("Final state:{}",global_state);
+}
