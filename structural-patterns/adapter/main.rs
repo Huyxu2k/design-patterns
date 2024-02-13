336d84a71d620c0319f3a8ab6c61a9f2ea8699c1
huyxu2k <nguyenhuy2712000@gmail.com>
2024-02-13 22:32:25 +0700

    adapter

---
+++ structural-patterns/adapter/main.rs
@@ -0,0 +1,30 @@
+mod target;
+mod adaptee;
+mod adapter;
+
+use adaptee::SpecificTarget;
+use adapter::TargetAdapter;
+use target::{OrdinaryTarget,Target};
+
+fn call(target: impl Target){
+    println!("'{}'",target.request());
+}
+
+fn main(){
+  let target=OrdinaryTarget;
+
+  print!("A compatible target can be directly called: ");
+  call(target);
+
+  let adaptee = SpecificTarget;
+
+    println!(
+        "Adaptee is incompatible with client: '{}'",
+        adaptee.specific_request()
+    );
+
+    let adapter = TargetAdapter::new(adaptee);
+
+    print!("But with adapter client can call its method: ");
+    call(adapter);
+}
---
+++ structural-patterns/adapter/target.rs
@@ -0,0 +1,10 @@
+pub trait Target {
+    fn request(&self)->String;
+}
+pub struct OrdinaryTarget;
+
+impl Target for OrdinaryTarget {
+    fn request(&self)->String {
+        "Ordinary requet.".into()
+    }
+}
